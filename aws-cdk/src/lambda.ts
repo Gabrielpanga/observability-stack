1ffb2a5c0394c95431cfdf122ac4aed72a084a4d
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2022-04-27 13:06:46 -0300

    chore: improve legibility.

--- aws-cdk/src/lambda.ts
+++ aws-cdk/src/lambda.ts
@@ -2,23 +2,31 @@ import Sentry from "@sentry/serverless"
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import axios from 'axios'
 
+const CURRENCY_API_URL = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json'
+
+function createHttpResponse(statusCode: number, body: Record<string, unknown>) {
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  };
+}
+
 const baseHandler: APIGatewayProxyHandlerV2 = async (event) => {
-  console.log('Processing request')
-  const request = event.queryStringParameters || {}
+  console.log(`Processing request: ${event.rawQueryString}`)
   
-  const { data: currencies } = await axios.get('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
-  console.log(`Found ${Object.keys(currencies).length} currencies`)
-  
-  if (request.shouldFail) {
-    console.log('When reviewing parameters, we found that the request should fail')
-    throw new Error('Super random error that was not captured by our APP')
+  let currencies
+  try {
+    const response = await axios.get(CURRENCY_API_URL)
+    currencies = response.data
+    console.log(`Found ${Object.keys(currencies).length} currencies`)
+  } catch (error) {
+    console.info(`Failed to fetch currencies, ${(error as Error).message}`)
+    console.error(error)
+    return createHttpResponse(500, { error: 'Failed to fetch currencies' })
   }
   
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(currencies),
-  };
+  return createHttpResponse(200, currencies);
 };
 
 export const handler: APIGatewayProxyHandlerV2 = Sentry.AWSLambda.wrapHandler(baseHandler);
